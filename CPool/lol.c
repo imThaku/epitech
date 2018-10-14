9fa5ae6c38464e03ba1cb254f95d161bbd3d3bf0
Unknown <stephane.dos96@gmail.com>
2018-10-14 11:41:43 +0200

    init repo

---
+++ CPool/lol.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2018
+** lol.c
+** File description:
+** description of the file
+*/
+
+
+src[1]= h;
+src[1][2]= h;
+src[1][3]= h;
+src[1][4]= h;
+
+while (x < 3){
+    while ( y < 4){
+       y++
+    }
+    x++
+}
+
+
+src[2][1]= h;
+src[2][2]= h;
+src[2][3]= h;
+
+src[3][1]= u;
+src[3][2]= u;
+src[3][3]= u;
